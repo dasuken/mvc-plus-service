d6cb726ceabdfe531a2ed7be0ce8060f2d1baeee
github daisukenoguchi <daisukenoguemon@gmail.com>
2021-05-10 22:15:21 +0900

    add service layer

---
+++ service/user.go
@@ -0,0 +1,18 @@
+package service
+
+import "github.com/noguchidaisuke/mvc-plus-service/model"
+
+// modelに対する処理を切り出す。
+func UpdateUserStatus(id int, name string) error {
+	// NewDbConnでconnection作成
+	// userオブジェクトは本来GetByIdから取得
+	user := model.User{
+		ID: id,
+		Name: name,
+	}
+
+	// 第一引数にconnectonを渡し、第２引数に取得したuserを渡す。
+	err := model.UpdateUser(&user)
+
+	return err
+}
